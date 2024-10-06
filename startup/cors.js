0aa871c4c0566e140959aa73394ac29ab053fa52
hesham.saleh <hesham.saleh@solve-it-solutions.com>
2024-10-06 18:59:43 +0300

    chaange cors

--- startup/cors.js
+++ startup/cors.js
@@ -1,8 +1,5 @@
 const cors = require("cors");
 
 module.exports = function(app) {
-  app.use(cors({
-    origin: 'http://localhost:3000', // Adjust to your frontend's URL
-    credentials: true,
-  }));
+  app.use(cors());
 };
