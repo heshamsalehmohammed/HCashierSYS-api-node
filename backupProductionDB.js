247166ce04d818a2ef4bd9113b8539793130dc23
Hesham Saleh <hesham.saleh@itworx.com>
2024-10-08 12:14:15 +0300

    fixes

--- backupProductionDB.js
+++ backupProductionDB.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 
 const {Customer} = require('./models/customer');
+const {User} = require('./models/user');
 const {StockItem} = require('./models/stockItem');
 const {Order} = require('./models/order');
+
 
 async function backupToLocal() {
   try {
     // Connect to production database
-    const prodConnection = await mongoose.createConnection('mongodb://localhost:27017/', {
+    const prodConnection = await mongoose.createConnection('mongodb://mongo:XZzxFWnpbsNcVoXUHNeleDgtpeVuaQVG@autorack.proxy.rlwy.net:49856/', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
@@ -15,9 +17,10 @@ async function backupToLocal() {
     const customers = await prodConnection.model('Customer').find();
     const stockItems = await prodConnection.model('StockItem').find();
     const orders = await prodConnection.model('Order').find();
+    const users = await prodConnection.model('User').find();
     
     // Connect to local database
-    const localConnection = await mongoose.createConnection('mongodb://mongo:XZzxFWnpbsNcVoXUHNeleDgtpeVuaQVG@autorack.proxy.rlwy.net:49856/', {
+    const localConnection = await mongoose.createConnection('mongodb://localhost:27017/', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
@@ -31,6 +34,9 @@ async function backupToLocal() {
     
     await localConnection.model('Order').deleteMany({});
     await localConnection.model('Order').insertMany(orders);
+
+    await localConnection.model('User').deleteMany({});
+    await localConnection.model('User').insertMany(users);
 
     console.info("Backup from production to local completed successfully!");
 
